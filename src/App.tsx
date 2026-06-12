ff13178059b759ceabbbf549fae295d8dca6b461
Anniken JE <AnnikenJE@gmail.com>
2026-06-12 19:35:23 +0200

    Add under construction landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import "./App.css";
+
 function App() {
-  return <></>;
+  return (
+    <main>
+      <h1>The Lionhearts</h1>
+      <p className="server">Darkmoon Faire — EU</p>
+      <p className="status">Website under construction</p>
+    </main>
+  );
 }
 
 export default App;
